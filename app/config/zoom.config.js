41e306bce028824b693f5e67413454e8e1f226c8
DESKTOP-R32AB8C\Comet <sunearth.moon@outlook.com>
2021-02-16 17:01:56 +0800

    update

--- app/config/zoom.config.js
+++ app/config/zoom.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ZOOM_CLIENT_ID: "obpOb1aCRUGwXqQaJhBd2A",
     ZOOM_CLIENT_SECRET: "qA35hDMmh05lFAxdktSMjgr0Vt4fUokM",
-    REDIRECT_URL: "http://localhost:3000/admin/dashboard",
+    REDIRECT_URL: "https://audaxfront.ukcourier.a2hosted.com/admin/dashboard",
     ZOOM_VERIFY_TOKEN: "ex2ITC8HT3-LzFVDJVwTiw"
 };
